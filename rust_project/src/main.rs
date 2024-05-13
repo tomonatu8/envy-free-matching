edfc26f62d5e16df6fd6ca702e49fda7dd3d0987
yokoyama <tomo_natu888@yahoo.co.jp>
2024-05-13 14:34:52 +0900

    add-inputs

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -19,34 +19,28 @@ use crate::matching::compute_max_weight_matching;
 
 
 
-fn create_wtr(n_each: usize, num_groups: usize, num_items: usize) -> (Writer<File>, Writer<File>){
-    let file_name_p = format!(
-        "outcome/outcome_p_{}_{}_{}.csv",
-        n_each,
-        num_groups,
-        num_items,
-    );
-    let file_out_p = fs::File::options()
-        .write(true)
-        .create(true)
-        .open(file_name_p)
-        .expect("CSV write failure");
-    let wtr_p = csv::Writer::from_writer(file_out_p);
-
-    let file_name_pq = format!(
-        "outcome/outcome_pq_{}_{}_{}.csv",
-        n_each,
-        num_groups,
-        num_items,
-    );
-    let file_out_pq = fs::File::options()
-        .write(true)
-        .create(true)
-        .open(file_name_pq)
-        .expect("CSV write failure");
-    let wtr_pq = csv::Writer::from_writer(file_out_pq);
-
-    (wtr_p, wtr_pq)
+fn create_wtr(n_each: usize, num_groups: usize, num_items: usize) -> (Writer<File>, Writer<File>, Writer<File>, Writer<File>){
+    let make_file = |str: String| -> Writer<File>{
+        let file_name = format!(
+            "outcome/outcome_{}_{}_{}_{}.csv",
+            str,
+            n_each,
+            num_groups,
+            num_items,
+        );
+        let file_out = fs::File::options()
+            .write(true)
+            .create(true)
+            .open(file_name)
+            .expect("CSV write failure");
+        let wtr = csv::Writer::from_writer(file_out);
+        wtr
+    };
+    let wtr_p = make_file("p".to_string());
+    let wtr_pq = make_file("pq".to_string());
+    let wtr_diff_p = make_file("diff_p".to_string());
+    let wtr_diff_pq = make_file("diff_pq".to_string());
+    (wtr_p, wtr_pq, wtr_diff_p, wtr_diff_pq)
 }
 
 fn create_groups_pref(n_each: usize, num_groups: usize, num_agents: usize, num_items: usize) -> (Vec<Vec<usize>>, Vec<Vec<f64>>) {
@@ -66,6 +60,15 @@ fn create_groups_pref(n_each: usize, num_groups: usize, num_agents: usize, num_i
 }
 
 
+fn calculate_diff (num_1:f64, num_2:f64) -> f64 {
+    if num_1 >= num_2 {
+        (num_1 - num_2).powi(2)
+    }
+    else {
+        0.0
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -78,7 +81,7 @@ fn main() {
     let num_agents = config.n_each * config.num_groups;
     let num_tries: usize = 100;
 
-    let (mut wtr_p, mut wtr_pq) = create_wtr(config.n_each, config.num_groups, config.num_items);
+    let (mut wtr_p, mut wtr_pq, mut wtr_diff_p, mut wtr_diff_pq) = create_wtr(config.n_each, config.num_groups, config.num_items);
 
     let pb = ProgressBar::new(num_tries as u64);
     
@@ -93,7 +96,7 @@ fn main() {
             config.num_items,
         );
         //println!("groups: {:?}", groups);
-        
+
         let (allocation, utility_list) = round_robin_allocation_by_group(
             config.num_items, 
             config.num_groups,
@@ -103,6 +106,77 @@ fn main() {
         );
         // println!("allocation \n {:?}", allocation);
         // println!("utility_list \n {:?}", utility_list);
+
+
+        let mut difference_p = 0.0;
+        let mut difference_pq = 0.0;
+        let mut count = 0;
+        for i in 0..num_agents {
+            for j in 0..config.num_items {
+
+                let mut another_preferences: Vec<Vec<f64>> = vec![Vec::new(); num_agents];
+
+                for k in 0..num_agents {
+                    another_preferences[k] = preferences[k].clone();
+                }
+                // println!("----{}, {}----",i,j);
+                another_preferences[i][j] = 0.0;
+
+                let (another_allocation, another_utility_list) = round_robin_allocation_by_group(
+                    config.num_items, 
+                    config.num_groups,
+                    config.n_each, 
+                    &groups, 
+                    &another_preferences,
+                );
+
+                // println!("another_allocation {:?}",another_allocation);
+
+                let mut count_bool = false;
+                //for p in 0..config.num_groups {
+                    //if p!=0 {continue;}
+                let each_diff_p = calculate_diff(utility_list[0], another_utility_list[0]);
+                difference_p += each_diff_p;
+                // println!("each_diff of {} : {}", p, each_diff_p);
+                if each_diff_p != 0.0 { count_bool = true };
+    
+                    //for q in 0..config.num_groups {
+                        //if q!=1 {continue;}
+                let (max_weight, assign) = compute_max_weight_matching(
+                    groups[0].clone(), 
+                    allocation[1].clone().into_iter().collect(), 
+                    &preferences, 
+                    config.n_each,
+                );
+
+                let (another_max_weight, another_assign) = compute_max_weight_matching(
+                    groups[0].clone(), 
+                    another_allocation[1].clone().into_iter().collect(), 
+                    &another_preferences, 
+                    config.n_each,
+                );
+                let each_diff_pq =  calculate_diff(max_weight, another_max_weight);
+                difference_pq += each_diff_pq;
+                        //println!("each_diff of {} toward {} : {}", p, q, each_diff_pq);
+                        // if q == (p + 1)%config.num_groups {
+                        //     let each_diff_pq = max_weight - another_max_weight;
+                        //     difference_pq += each_diff_pq;
+                        //     println!("each_diff of {} toward {} : {}", p, q, each_diff_pq);
+                        // }
+                    
+                if count_bool {
+                    count += 1;
+                }
+            }
+        }
+        println!("count : {}", count);
+        // if count > num_agents{
+        //     break;
+        // }
+
+        println!("difference_p, difference_pq : {}, {}", difference_p, difference_pq);
+        wtr_diff_p.serialize(difference_p).expect("CSV write failure");
+        wtr_diff_pq.serialize(difference_pq).expect("CSV write failure");
         
         for p in 0..config.num_groups {
             // println!("----------Class {} evaluates class {} 's bundle as {}.", p, p, utility_list[p]);
